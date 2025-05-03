65d74c965c198b7a6ff87b0b02175aba3327ddd2
ibrahimcs51 <engribrahimkhanokz@gmail.com>
2025-05-03 22:51:46 +0500

    add functiom method

--- App.js
+++ App.js
@@ -1,4 +1,25 @@
-var date=new Date()
+// var date=new Date()
+// var days=["sun" ,"mon","tue","wed","thu","fri","sat"]
 
-console.log(date.getDate())
+// console.log(days[date.getDate()])
+
+
+// var months= new months()
+
+// var totalMonths=["January","February","March","April","May","June","July",
+//     "August","September","October","November","December"];
+//     console.log(totalMonths[months.getMonth()])
+
+function abc(){
+    console.log(new Date)
+}
+abc();
+    
+function myFunction(num,num2){
+console.log (num+num2)
+}
+myFunction(2,2)
+
+
+
 
